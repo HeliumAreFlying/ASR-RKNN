157ba623bf2aac5e767937fd94c2ad5243a46aa2
user_10828615 <1436980420@qq.com>
2026-01-31 14:31:55 +0800

    fix bug

--- utils/get_dataset.py
+++ utils/get_dataset.py
@@ -1,5 +1,8 @@
 from modelscope.msdatasets import MsDataset
 
 if __name__ == '__main__':
-    ds =  MsDataset.load('speechoceanadmin/ChineseMandarinSpeechRecognitionCorpus-Mobile', subset_name='default', split='train')
+    ds =  MsDataset.load(dataset_name='speechoceanadmin/ChineseMandarinSpeechRecognitionCorpus-Mobile',
+                         cache_dir="training_cache",
+                         subset_name='default',
+                         split='train')
     pass
