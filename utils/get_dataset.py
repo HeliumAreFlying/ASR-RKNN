c4835e995aa75588798c88fb577b924ad2f1840c
user_10828615 <1436980420@qq.com>
2026-01-31 14:33:15 +0800

    make dir

--- utils/get_dataset.py
+++ utils/get_dataset.py
@@ -3,6 +3,7 @@ from modelscope.msdatasets import MsDataset
 
 if __name__ == '__main__':
     cache_dir = os.path.join(os.getcwd(), 'training_cache')
+    os.makedirs(cache_dir, exist_ok=True)
     ds =  MsDataset.load(dataset_name='speechoceanadmin/ChineseMandarinSpeechRecognitionCorpus-Mobile',
                          cache_dir=cache_dir,
                          subset_name='default',
