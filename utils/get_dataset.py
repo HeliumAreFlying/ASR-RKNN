27d19dec188309b492e9faae0aec03ef8d879c3d
user_10828615 <1436980420@qq.com>
2026-01-31 14:25:34 +0800

    dataset

--- utils/get_dataset.py
+++ utils/get_dataset.py
@@ -2,4 +2,4 @@ from modelscope.msdatasets import MsDataset
 
 if __name__ == '__main__':
     ds =  MsDataset.load('speechoceanadmin/ChineseMandarinSpeechRecognitionCorpus-Mobile', subset_name='default', split='train')
-    
+    pass
