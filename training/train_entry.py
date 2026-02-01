749c7182b3aa4f8da26b22bcd18919ff20ac1e28
17629077396 <1436980420@qq.com>
2026-02-02 06:18:07 +0800

    64

--- training/train_entry.py
+++ training/train_entry.py
@@ -13,7 +13,7 @@ from main_model import TDNNASR, public_device
 from utils.get_feat import load_and_resample_audio, compute_feat
 
 TRAINING_DATA_ROOT = "D:/zhvoice"
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 LEARNING_RATE = 2.5e-4
 NUM_EPOCHS = 10000
 VAL_SPLIT = 0.1
