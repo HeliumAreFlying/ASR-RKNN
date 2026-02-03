ed6f3f81c83041edb92d243d54525c44079c3a2e
17629077396 <1436980420@qq.com>
2026-02-03 21:37:24 +0800

    new model

--- training/main_model.py
+++ training/main_model.py
@@ -178,10 +178,10 @@ if __name__ == "__main__":
 
     model = TDNNASR(
         input_dim=80,
-        block_dims=[384] * 18,
-        dilations=[1, 2, 4, 2, 4, 4, 2, 2, 1, 2, 4, 2, 4, 4, 2, 2, 1, 1],
-        strides=[1] * 17 + [2],
-        proj_dim=384,
+        block_dims=[512] * 18,
+        dilations=[1,2,4,4,2,1] * 3,
+        strides=[1,1,1] * 2 + [1,1,2] * 4,
+        proj_dim=512,
         num_classes=vocab_data['vocab_size'] + 1,
         vocab_data=vocab_data,
         max_window_size=512,
--- training/train_entry.py
+++ training/train_entry.py
@@ -96,10 +96,10 @@ def train():
 
     model = TDNNASR(
         input_dim=80,
-        block_dims=[384] * 18,
-        dilations=[1, 2, 4, 2, 4, 4, 2, 2, 1, 2, 4, 2, 4, 4, 2, 2, 1, 1],
-        strides=[1] * 17 + [2],
-        proj_dim=384,
+        block_dims=[512] * 18,
+        dilations=[1,2,4,4,2,1] * 3,
+        strides=[1,1,1] * 2 + [1,1,2] * 4,
+        proj_dim=512,
         num_classes=num_classes,
         vocab_data=dataset.vocab
     ).to(public_device)
